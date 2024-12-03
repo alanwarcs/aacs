20398c08861458e02d1c70bdec11d4bd5e6befe6
Murtaza Patel <imurtaza97@gmail.com>
2024-12-03 16:33:55 +0530

    Initial commit of ASP.NET MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aacs.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// Typing effect for the heading (h6)
+function typeText(elementId, text, speed) {
+    let i = 0;
+    let element = document.getElementById(elementId);
+    element.innerHTML = ''; // Clear any existing text
+    let interval = setInterval(function () {
+        element.innerHTML += text.charAt(i);
+        i++;
+        if (i === text.length) {
+            clearInterval(interval);
+        }
+    }, speed);
+}
+
+// On page load, trigger the typing effects
+window.onload = function () {
+    typeText("typing-effect-heading", "Welcome to Al Anwar Creativity Studio,", 70); // Adjust typing speed here (in milliseconds)
+};
